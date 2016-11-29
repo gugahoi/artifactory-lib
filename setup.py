0dce147b06543ef104830b57cf5da5d64f3d482a
Gustavo Hoirisch <guga@maurorisch.com.br>
2016-11-30 09:22:41 +1100

    only support python 2.7 and remove travis build file

--- setup.py
+++ setup.py
@@ -33,11 +33,6 @@ setup(
 
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 
     # What does your project relate to?
