bcbb567c7ac8249d86e8ba0ba3cef0dc37e7ca39
Gustavo Hoirisch <guga@maurorisch.com.br>
2016-11-30 08:52:31 +1100

    dynamic pipeline so theres only deployment on tags

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cat << EOF
 steps:
   - label: 'Testing :python:'
     command: 'python -m unittest discover'
@@ -5,8 +10,14 @@ steps:
       queue: 'voyager'
     env:
       BUILDKITE_DOCKER: true
+EOF
 
+# deploy only if it's a tag
+if [[ -n "${BUILDKITE_TAG:-}" ]]; then
+  cat << EOF
   - label: 'Deploying to PyPi'
     command: ./deploy.sh
     agents:
       queue: 'voyager'
+EOF
+fi
